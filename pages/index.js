d29b7ac50cee3328a72162c57b80ccbc4d89ae34
Johnny Wu <64444867+johnnywu-namebase@users.noreply.github.com>
2020-11-18 14:26:39 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://namebase.io">my home page!</a>
         </h1>
 
         <p className={styles.description}>
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>Let's chat!</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
